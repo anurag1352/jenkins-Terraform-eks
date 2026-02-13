1af951be86d3b842b933781b8c64a4fca8c369c3
Anurag <anuragsharma921998@gmail.com>
2026-02-13 12:00:03 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,16 @@
-locals {
-  region   = "us-east-1"
-  name     = "eks-ka-cluster"
-  vpc_cidr = "10.123.0.0/16"
+terraform {
+  required_version = ">= 1.3.0"
 
-  azs = ["us-east-1a", "us-east-1b"]
-
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-
-  tags = {
-    Name        = local.name
-    Environment = "dev"
-    Terraform   = "true"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
   }
 }
+
+provider "aws" {
+  region = local.region
+}
+
 
