6c3d04de319471bc788663f32b8e0d74456f070f
Anurag <anuragsharma921998@gmail.com>
2026-02-13 11:55:28 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
+  version = "5.1.2"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -11,12 +11,18 @@ module "vpc" {
   intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
+  single_nat_gateway = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${local.name}" = "shared"
   }
+
+  tags = local.tags
 }
+
